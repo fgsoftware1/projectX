672c2df6081a6654db05e0ff99c0892b81520760
fgsoftware1 <fgsoftwarestudio@gmail.com>
2021-05-07 22:28:43 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('compile-less',async function(){
     return gulp
-    .src('./www/dist/private/main.less')
+    .src(['./www/dist/private/*.less','./www/dist/private/*.less'])
     .pipe(gulpless())
     .pipe(gulp.dest('www/tmp'))
 });
 
 gulp.task('compile-coffee',async function(){
-    gulp.src('./www/dist/private/scripts/*.coffee')
+    gulp.src(['./www/dist/private/*.coffee','./www/dist/private/scripts/*.coffee'])
     .pipe(coffee())
     .pipe(gulp.dest('./www/tmp'));
 })
