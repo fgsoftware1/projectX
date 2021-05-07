63df613e7ed19d0d8bbf2b804b9cea0d1c62f380
fgsoftware1 <fgsoftwarestudio@gmail.com>
2021-05-07 21:47:08 +0100

    minifying files mapping files

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,35 @@
 var gulp = require('gulp');
 var gulpless = require('gulp-less');
+var coffee = require('gulp-coffee');
 var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('compile-less',function(){
-
-var srcfile = './www/dist/private/main.less';
-var tmp = './www/temp/main.css';
-
-return gulp
-    .src(srcfile)
+gulp.task('compile-less',async function(){
+    return gulp
+    .src('./www/dist/private/main.less')
     .pipe(gulpless())
-    .pipe(gulp.dest(dest))
+    .pipe(gulp.dest('www/tmp'))
 });
 
-gulp.task('minify-css', () => {
-    return gulp.src('./www/dist/tmp/main.css')
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest('./www/dist/public/main.css'));
+gulp.task('minify-css',async function(){
+    return gulp.src('www/tmp/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('mapping'))
+    .pipe(gulp.dest('./www/dist/public/styles'));
+});
+
+gulp.task('compile-coffee',async function(){
+    gulp.src('./www/dist/private/scripts/*.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('./www/tmp'));
+})
+
+gulp.task('minify-js',async function() {
+        gulp.src('www/tmp/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('mapping'))
+        .pipe(gulp.dest('./www/dist/public/scripts'))
 });
