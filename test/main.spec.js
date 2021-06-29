a4754ed92da1fcc545e9e8985c6bd9712e3ee089
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-06-29 21:21:37 +0100

    Update main.spec.js

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,5 @@
-import { assert } from 'chai';
-import app from './main';
+const assert = require('chai').assert;
+const app = require('./main');
 
 describe('App', function(){
     it('app should return hello', function(){
