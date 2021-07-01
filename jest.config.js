337af6815f97e57d8af666f650e60802cd44081c
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-07-01 09:53:57 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,36 @@
 module.exports = {
   collectCoverage: true,
-  verbose: true,
   coverageDirectory: "coverage",
-  coerageReporters: [
-  "json",
-  "text",
-  "lcov",
-  "html"
+  coveragePathIgnorePatterns: [
+  "\\\\node_modules\\\\"
   ],
+  coverageReporters: [
+  "json",
+  "html",
+  "lcov"
+  ],
+  moduleDirectories: [
+  "node_modules"
+  ],
+  moduleFileExtensions: [
+  "js",
+  "jsx",
+  "ts",
+  "tsx",
+  "json",
+  "node"
+  ],
+  reporters: [
+    "default"
+  ],
+  rootDir: ".",
+  runner: "jest-runner",
+  testEnvironment: "jest-environment-node",
+  testPathIgnorePatterns: [
+  "\\\\node_modules\\\\"
+  ],
+  testRunner: "jest-circus/runner",
+  testURL: "http://localhost:8080",
+  verbose: true,
+  watchman: true,
 };
